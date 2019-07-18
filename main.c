72bf7a6acbd105d8ee21c1a290fdd133854a7637
iamkroot <kroot.patel@gmail.com>
2019-07-18 10:40:26 +0530

    Add basic vm

--- main.c
+++ main.c
@@ -2,15 +2,19 @@
 
 #include "chunk.h"
 #include "debug.h"
+#include "vm.h"
 
 int main() {
+    initVM();
     Chunk chunk;
     initChunk(&chunk);
     int constant = addConstant(&chunk, 2.79);
     writeChunk(&chunk, OP_CONSTANT, 123);
     writeChunk(&chunk, constant, 123);
     writeChunk(&chunk, OP_RETURN, 123);
-    disassembleChunk(&chunk, "test chunk");
+//    disassembleChunk(&chunk, "test chunk");
+    interpret(&chunk);
+    freeVM();
     freeChunk(&chunk);
     return 0;
 }
