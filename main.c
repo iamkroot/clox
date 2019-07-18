6f13c6b393ba1e8fc3bc7f319df3175040d5ff77
iamkroot <kroot.patel@gmail.com>
2019-07-18 11:12:59 +0530

    Add basic arithmetic operators

--- main.c
+++ main.c
@@ -11,6 +11,19 @@ int main() {
     int constant = addConstant(&chunk, 2.79);
     writeChunk(&chunk, OP_CONSTANT, 123);
     writeChunk(&chunk, constant, 123);
+    constant = addConstant(&chunk, 3.4);
+    writeChunk(&chunk, OP_CONSTANT, 123);
+    writeChunk(&chunk, constant, 123);
+
+    writeChunk(&chunk, OP_ADD, 123);
+
+    constant = addConstant(&chunk, 5.6);
+    writeChunk(&chunk, OP_CONSTANT, 123);
+    writeChunk(&chunk, constant, 123);
+
+    writeChunk(&chunk, OP_DIVIDE, 123);
+    writeChunk(&chunk, OP_NEGATE, 123);
+
     writeChunk(&chunk, OP_RETURN, 123);
 //    disassembleChunk(&chunk, "test chunk");
     interpret(&chunk);
