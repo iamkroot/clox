6407f9f5cc49e4a41a5431c4e72222a6bc6998e8
iamkroot <kroot.patel@gmail.com>
2019-07-25 18:59:35 +0530

    Add nil and boolean Values and operators

--- compiler.c
+++ compiler.c
@@ -145,6 +145,18 @@ static void binary() {
     parsePrecedence(rule->precedence + 1);
 
     switch (operatorType) {
+        case TOKEN_BANG_EQUAL: emitBytes(OP_EQUAL, OP_NOT);
+            break;
+        case TOKEN_EQUAL_EQUAL: emitByte(OP_EQUAL);
+            break;
+        case TOKEN_GREATER: emitByte(OP_GREATER);
+            break;
+        case TOKEN_GREATER_EQUAL: emitBytes(OP_LESS, OP_NOT);
+            break;
+        case TOKEN_LESS: emitByte(OP_LESS);
+            break;
+        case TOKEN_LESS_EQUAL: emitBytes(OP_GREATER, OP_NOT);
+            break;
         case TOKEN_PLUS: emitByte(OP_ADD);
             break;
         case TOKEN_MINUS: emitByte(OP_SUBTRACT);
@@ -152,6 +164,18 @@ static void binary() {
         case TOKEN_STAR: emitByte(OP_MULTIPLY);
             break;
         case TOKEN_SLASH: emitByte(OP_DIVIDE);
+            break;
+        default: return;
+    }
+}
+
+static void literal() {
+    switch (parser.previous.type) {
+        case TOKEN_FALSE: emitByte(OP_FALSE);
+            break;
+        case TOKEN_TRUE: emitByte(OP_TRUE);
+            break;
+        case TOKEN_NIL: emitByte(OP_NIL);
             break;
         default: return;
     }
@@ -171,6 +195,8 @@ static void unary() {
     TokenType operatorType = parser.previous.type;
     parsePrecedence(PREC_UNARY);
     switch (operatorType) {
+        case TOKEN_BANG: emitByte(OP_NOT);
+            break;
         case TOKEN_MINUS: emitByte(OP_NEGATE);
             break;
         default: return;
@@ -184,19 +210,19 @@ ParseRule rules[] = {
         {NULL,     NULL, PREC_NONE},       // TOKEN_RIGHT_BRACE
         {NULL,     NULL, PREC_NONE},       // TOKEN_COMMA
         {NULL,     NULL, PREC_CALL},       // TOKEN_DOT
-        {unary, binary,  PREC_TERM},       // TOKEN_MINUS
-        {NULL,  binary,  PREC_TERM},       // TOKEN_PLUS
+        {unary,  binary, PREC_TERM},       // TOKEN_MINUS
+        {NULL,   binary, PREC_TERM},       // TOKEN_PLUS
         {NULL,     NULL, PREC_NONE},       // TOKEN_SEMICOLON
-        {NULL,  binary,  PREC_FACTOR},     // TOKEN_SLASH
-        {NULL,  binary,  PREC_FACTOR},     // TOKEN_STAR
-        {NULL,     NULL, PREC_NONE},       // TOKEN_BANG
-        {NULL,     NULL, PREC_EQUALITY},   // TOKEN_BANG_EQUAL
+        {NULL,   binary, PREC_FACTOR},     // TOKEN_SLASH
+        {NULL,   binary, PREC_FACTOR},     // TOKEN_STAR
+        {unary,    NULL, PREC_NONE},       // TOKEN_BANG
+        {NULL,   binary, PREC_EQUALITY},   // TOKEN_BANG_EQUAL
         {NULL,     NULL, PREC_NONE},       // TOKEN_EQUAL
-        {NULL,     NULL, PREC_EQUALITY},   // TOKEN_EQUAL_EQUAL
-        {NULL,     NULL, PREC_COMPARISON}, // TOKEN_GREATER
-        {NULL,     NULL, PREC_COMPARISON}, // TOKEN_GREATER_EQUAL
-        {NULL,     NULL, PREC_COMPARISON}, // TOKEN_LESS
-        {NULL,     NULL, PREC_COMPARISON}, // TOKEN_LESS_EQUAL
+        {NULL,   binary, PREC_EQUALITY},   // TOKEN_EQUAL_EQUAL
+        {NULL,   binary, PREC_COMPARISON}, // TOKEN_GREATER
+        {NULL,   binary, PREC_COMPARISON}, // TOKEN_GREATER_EQUAL
+        {NULL,   binary, PREC_COMPARISON}, // TOKEN_LESS
+        {NULL,   binary, PREC_COMPARISON}, // TOKEN_LESS_EQUAL
         {NULL,     NULL, PREC_NONE},       // TOKEN_IDENTIFIER
         {NULL,     NULL, PREC_NONE},       // TOKEN_STRING
         {number,   NULL, PREC_NONE},       // TOKEN_NUMBER
@@ -204,16 +230,16 @@ ParseRule rules[] = {
         {NULL,     NULL, PREC_NONE},       // TOKEN_CLASS
         {NULL,     NULL, PREC_NONE},       // TOKEN_DEF
         {NULL,     NULL, PREC_NONE},       // TOKEN_ELSE
-        {NULL,     NULL, PREC_NONE},       // TOKEN_FALSE
+        {literal,  NULL, PREC_NONE},       // TOKEN_FALSE
         {NULL,     NULL, PREC_NONE},       // TOKEN_FOR
         {NULL,     NULL, PREC_NONE},       // TOKEN_IF
-        {NULL,     NULL, PREC_NONE},       // TOKEN_NIL
+        {literal,  NULL, PREC_NONE},       // TOKEN_NIL
         {NULL,     NULL, PREC_OR},         // TOKEN_OR
         {NULL,     NULL, PREC_NONE},       // TOKEN_PRINT
         {NULL,     NULL, PREC_NONE},       // TOKEN_RETURN
         {NULL,     NULL, PREC_NONE},       // TOKEN_SUPER
         {NULL,     NULL, PREC_NONE},       // TOKEN_THIS
-        {NULL,     NULL, PREC_NONE},       // TOKEN_TRUE
+        {literal,  NULL, PREC_NONE},       // TOKEN_TRUE
         {NULL,     NULL, PREC_NONE},       // TOKEN_VAR
         {NULL,     NULL, PREC_NONE},       // TOKEN_WHILE
         {NULL,     NULL, PREC_NONE},       // TOKEN_ERROR
