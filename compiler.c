3075665e0b82a2efc29741bd2c6c8d192e116d36
iamkroot <kroot.patel@gmail.com>
2019-09-19 14:46:50 +0530

    Add return statements

--- compiler.c
+++ compiler.c
@@ -620,6 +620,19 @@ static void printStatement() {
     emitByte(OP_PRINT);
 }
 
+static void returnStatement() {
+    if (current->type == TYPE_SCRIPT) {
+        error("Cannot return from top-level code.");
+    }
+    if(match(TOKEN_SEMICOLON)) {
+        emitReturn();
+    } else {
+        expression();
+        consume(TOKEN_SEMICOLON, "Expect ';' after return value.");
+        emitByte(OP_RETURN);
+    }
+}
+
 static void whileStatement() {
     int loopStart = currentChunk()->count;
     consume(TOKEN_LEFT_PAREN, "Expect '(' after 'while'.");
@@ -668,7 +681,9 @@ static void statement() {
         printStatement();
     } else if (match(TOKEN_IF)) {
         ifStatement();
-    } else if (match(TOKEN_WHILE)) {
+    }   else if (match(TOKEN_RETURN)) {
+        returnStatement();
+    }else if (match(TOKEN_WHILE)) {
         whileStatement();
     } else if (match(TOKEN_FOR)) {
         forStatement();
